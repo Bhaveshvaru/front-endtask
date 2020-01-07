61f567f24330794380e0ddf1b2156bc47b758801
bhavesh varu <bhavesh.varu97@gmail.com>
2020-01-08 01:29:08 +0530

    added doughnut chart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./App.css";
 import {
   LineChart,
   Line,
@@ -8,28 +9,76 @@ import {
   Tooltip,
   Legend
 } from "recharts";
+import { PieChart, Pie, Sector, Cell } from "recharts";
+
+const data = [
+  { name: "Group A", value: 90 },
+  { name: "Group B", value: 70 },
+  { name: "Group C", value: 30 }
+];
+const COLORS = ["#E8290B", "#FFF222", "#2ecc72"];
 
 class App extends Component {
   render() {
     return (
-      <LineChart
-        width={500}
-        height={400}
-        data={this.state.items}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="createdAt" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="avgSpeed" stroke="#25CCF7" />
-      </LineChart>
+      <div>
+        <div>
+          <div>
+            <div>
+              <p class="hh"> ROAD SAFTY ANALYTICS</p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <LineChart
+                width={700}
+                height={450}
+                data={this.state.items}
+                margin={{
+                  top: 25,
+                  right: 30,
+                  left: 10,
+                  bottom: 5
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="createdAt" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="avgSpeed" stroke="#25CCF7" />
+              </LineChart>
+            </div>
+            <div>
+              <PieChart
+                className="bb"
+                width={800}
+                height={400}
+                onMouseEnter={this.onPieEnter}
+              >
+                <Pie
+                  data={this.state.items}
+                  cx={120}
+                  cy={200}
+                  innerRadius={60}
+                  outerRadius={80}
+                  fill="#8884d8"
+                  paddingAngle={5}
+                  dataKey="total_travel_duration"
+                >
+                  {data.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+              </PieChart>{" "}
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
   constructor(props) {
